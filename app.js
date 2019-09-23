89115922eed93c6508ab9c3906158121184f45f7
Steven Bull <stevenwbulljr@gmail.com>
2019-09-23 16:55:27 -0500

    add sort functionality to app.js

--- app.js
+++ app.js
@@ -8,8 +8,23 @@ app.use(morgan('common'));
 const books = require('./books-data');
 
 app.get('/books', (req, res) => {
-  const { search = ''} = req.query;
+  const { search = '', sort } = req.query;
+
+  if(sort) {
+    if(!['title', 'rank'].includes(sort)) {
+      return res
+        .status(400)
+        .send('Sort must be one of title or rank')
+    }
+  };
+
   let results = books.filter(book => book.title.toLowerCase().includes(search.toLowerCase()));
+
+  if(sort) {
+    results.sort(( a, b ) => {
+      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    })
+  }
 
   res.json(results);
 });
